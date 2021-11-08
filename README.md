29c517483b8b0226bcbc8559e58d56fdf6095043
Muhammad Ridho Anshory <muhridhoanshory@gmail.com>
2021-11-08 18:12:00 +0800

    add counter

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-interview-practice
+
 Created with CodeSandbox
+
+## Interview Questions
+
+1. Create a counter and a button to increase the counter / decrease the counter
+
+2. fetch a data from API and display it to the web pages
--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+
+      <h4>{counter}</h4>
+      <button onClick={increase}>Increase</button>
     </div>
   );
 }
